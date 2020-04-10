77dba722bfb2d18d8acb0aaf82601989fe48722b
YannisDelmas <github-a-yd@tyds.info>
2020-04-10 17:30:40 +0200

    correction balise body

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ header('Content-Type: text/html; charset=utf-8');
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
 	<link rel="stylesheet" href="explain-expression.css">
 </head>
-<body lang="<?= $lang ?>">
+<body class="module-<?= $module ?>">
 	<header>
 		<h1><?= $interface['docTitle'] ?></h1>
 		<div class="chapeau"><?= $interface['info'] ?></div>
